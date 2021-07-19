3ea6536ef1daa7007b2ba2f82385c744ff8cd7e9
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2021-07-19 09:46:18 +0300

    Add files via upload

---
+++ EBSRunADminTools/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace EBSRunADminTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ProcessStartInfo processStartInfo;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            yoneticiexcute(@"C:\Windows\System32\ctfmon.exe");
+        }
+
+        private void yoneticiexcute(string yol)
+        {
+          processStartInfo = new  ProcessStartInfo(yol, "args");
+            processStartInfo.Verb = "runas";
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = processStartInfo;
+                process.Start();
+            }
+        }
+    }
+}
